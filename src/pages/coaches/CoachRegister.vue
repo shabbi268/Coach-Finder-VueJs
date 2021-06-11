99a2bac124f4c1701d7061e08b2fbe57bc58ea2e
Kesa Shabarish <shabarish.shabbi@gmail.com>
2021-06-11 19:12:18 -0400

    coach register form developed

--- src/pages/coaches/CoachRegister.vue
+++ src/pages/coaches/CoachRegister.vue
@@ -1,3 +1,24 @@
 <template>
-    <p>Register</p>
+  <section>
+    <base-card>
+      <h2>Register as a Coach</h2>
+      <coach-form @register-form="addCoach"></coach-form>
+    </base-card>
+  </section>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+
+export default {
+  components: { CoachForm },
+  methods: {
+      addCoach(formData) {
+          this.$store.actions.dispatch('add-coach', formData);
+      }
+  }
+};
+</script>
+
+<style scoped>
+</style>
