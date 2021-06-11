946afb7280ff491a468e1f38fefca9c65520fca9
Kesa Shabarish <shabarish.shabbi@gmail.com>
2021-06-11 15:50:33 -0400

    component files and linked to router

--- src/router.js
+++ src/router.js
@@ -1,4 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegister from './pages/coaches/CoachRegister.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -9,29 +15,29 @@ const router = createRouter({
         },
         {
             path: '/coaches',
-            component: null
+            component: CoachesList
         },
         {
             path: '/coaches/:id',
-            component: null,
+            component: CoachDetail,
             children: [
                 {
                     path: 'contact',
-                    component: null
+                    component: ContactCoach
                 }
             ]
         },
         {
             path: '/register',
-            component: null
+            component: CoachRegister
         },
         {
             path: '/requests',
-            component: null
+            component: RequestsReceived
         },
         {
             path: ':notFound(.*)',
-            component: null
+            component: NotFound
         }
     ]
 });
